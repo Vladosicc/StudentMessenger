32cc1d76121226507c0be552510c21d4b8de1fb7
Виктор <deather52rus@yandex.ru>
2023-03-18 02:41:37 +0300

    - added json serialization - fixed ip not changed in WPF form - fixed many things - Client_Wpf might be now correct

--- Client_WPF/Windows/MainWindow.xaml.cs
+++ Client_WPF/Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -16,6 +17,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NetDataLibrary;
+
+using Newtonsoft.Json;
+
 namespace Client_WPF.Windows
 {
     /// <summary>
@@ -25,20 +30,21 @@ namespace Client_WPF.Windows
     {
         public AppNetwork Network { get; set; } = new AppNetwork();
 
+        public ClientsData ClData { get; set; } = new ClientsData();
+
         public MainWindow()
         {
             InitializeComponent();
-            Network.OnNetworkStatusChanged += UI_NetStatusChanged;
         }
 
         private void Window_Initialized(object sender, EventArgs e)
         {
-            
+            LB_Main_Chats.ItemsSource = ClData.Clients;     
         }
 
         private void UI_NetStatusChanged(ServerStatus status, IPNetData data)
         {
-            UI_ChangeStatus(status, data);
+            Dispatcher.Invoke(() => UI_ChangeStatus(status, data));
         }
 
         private void UI_ChangeStatus(ServerStatus status, IPNetData data)
@@ -73,6 +79,11 @@ namespace Client_WPF.Windows
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            TryConnectToServer();
+        }
+
+        private void TryConnectToServer()
+        {
             if (Network.IsEmpty)
             {
                 Window_IPEntering w_Ip = new Window_IPEntering();
@@ -81,7 +92,11 @@ namespace Client_WPF.Windows
                 {
                     try
                     {
-                        Network = AppNetwork.OpenConnectionTo(w_Ip.IpPort, w_Ip.UserName);
+                        Network = AppNetwork.OpenConnectionTo(w_Ip.IpPort);
+                        Network.OnNetworkStatusChanged += UI_NetStatusChanged;
+                        Network.OnClientStatusChanged += Network_OnClientStatusChanged;
+                        Network.OnClientReceivedMessage += Network_OnClientReceivedMessage;
+                        Network.OpenSocketWithData(w_Ip.UserName);
                     }
                     catch (Exception ex)
                     {
@@ -89,6 +104,139 @@ namespace Client_WPF.Windows
                     }
                 }
             }
+        }
+
+        private void Network_OnClientReceivedMessage(ushort recepientID, ushort receiverID, string message)
+        {
+            ClData.AddMessageToChat(recepientID, receiverID, message);
+        }
+
+        private void Network_OnClientStatusChanged(ushort recID, string userLogin, ClientStatus status)
+        {
+            if (status == ClientStatus.Connected)
+            {
+                try
+                {
+                    ClData.AddClient(new Client(recID, userLogin));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка." + ex.Message, "Произошла ошибка.");
+                }
+            }
+            else if (status == ClientStatus.Disconnected)
+            {
+                try
+                {
+                    if (!ClData.RemoveClient(recID))
+                    {
+                        MessageBox.Show("Ошибка при удалении клиента с ID - " + recID + ". ", "Произошла ошибка.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка. " + ex.Message, "Произошла ошибка.");
+                }
+
+            }
+        }
+
+        private void LB_Main_Chats_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var index = LB_Main_Chats.SelectedIndex;
+            try
+            {
+                var client = ClData.FindClientWithIndex(index);
+                if (client != null)
+                {
+                    LB_Main_Messages.ItemsSource = client.Messages;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка. " + ex.Message);
+            }
+            
+        }
+
+        private void MI_Server_ChangeServ_Click(object sender, RoutedEventArgs e)
+        {
+            DisconnectClient();
+            TryConnectToServer();
+        }
+
+        private void MI_Server_Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            DisconnectClient();
+        }
+
+        private void DisconnectClient()
+        {
+            Network.DisconnectClient();
+            Network.OnNetworkStatusChanged -= UI_NetStatusChanged;
+            Network.OnClientStatusChanged -= Network_OnClientStatusChanged;
+            Network.OnClientReceivedMessage -= Network_OnClientReceivedMessage;
+        }
+    }
+
+    public class ClientsData
+    {
+        public ObservableCollection<Client> Clients { get; set; } = new ObservableCollection<Client>();
+
+        public ClientsData ()
+        {
+            Clients = new ObservableCollection<Client>();
+        }
+
+        public void AddClient(Client client)
+        {
+            if (FindClientWithID(client.ClientID) == null)
+            {
+                Clients.Add(client);
+            }
+            else
+                throw new Exception("Пользователь с ID = " + client.ClientID + "уже существует.");
+        }
+
+        public bool RemoveClient(ushort clID)
+        {
+            var client = FindClientWithID(clID);
+            if (client != null)
+            {
+                return Clients.Remove(client);
+            }
+            else
+                throw new Exception("Пользователь с ID " + clID + "не был найден.");
+        }
+
+        public Client FindClientWithID(ushort clID)
+        {
+            if (Clients == null)
+            {
+                Clients = new ObservableCollection<Client>();
+            }
+            if (Clients.Count == 0)
+                return null;
+            return Clients.FirstOrDefault(cl => cl.ClientID == clID);
+        }
+
+        public void AddMessageToChat (ushort recepientID, ushort receiverID, string message)
+        {
+            var client = FindClientWithID(recepientID);
+            if (client != null)
+            {
+                client.AddMessage(recepientID, receiverID, message);
+            }
+        }
+
+        public Client FindClientWithIndex(int index)
+        {
+            if (index < Clients.Count)
+            {
+                return Clients[index];
+            }
+            else
+                throw new Exception("Невозможно найти клиента с выбранным индексом.");
         }
 
     }
@@ -103,7 +251,7 @@ namespace Client_WPF.Windows
         { 
             get
             {
-                return IP != null;
+                return IP == null;
             }
         }
 
@@ -159,16 +307,28 @@ namespace Client_WPF.Windows
 
         public event NetworkStatusChanged OnNetworkStatusChanged;
 
+        public delegate void ClientStatusChanged(ushort recID, string userLogin, ClientStatus status);
+
+        public event ClientStatusChanged OnClientStatusChanged;
+
+        public delegate void ClientReceivedMessage(ushort recepientID, ushort receiverID, string message);
+
+        public event ClientReceivedMessage OnClientReceivedMessage;
+
         private TcpClient SocketClient { get; set; } = new TcpClient();
 
         public IPNetData MainData { get; set; }
 
-        public AppNetwork()
+        public bool? IsConnected
         {
-                
+            get
+            {
+                if (MainData == null || SocketClient == null)
+                    return null;
+                return
+                    SocketClient?.Connected;
+            }
         }
-
-        MemoryStream MemStream = null;
 
         public bool IsEmpty 
         { 
@@ -180,24 +340,32 @@ namespace Client_WPF.Windows
             } 
         }
 
-        private AppNetwork(IPNetData data, string userName)
+        public AppNetwork()
+        {
+                
+        }
+
+        MemoryStream MemStream = null;
+
+        private AppNetwork(IPNetData data)
         {
             MainData = data;
+            //OpenSocketWithData(userName);
         }
 
-        private AppNetwork(string ipPort, string userName)
+        private AppNetwork(string ipPort)
         {
             MainData = new IPNetData(ipPort);
-            OpenSocketWithData(userName);
+            //OpenSocketWithData(userName);
         }
 
-        private AppNetwork(string ip, ushort port, string userName)
+        private AppNetwork(string ip, ushort port)
         {
             MainData = new IPNetData(ip, port);
-            OpenSocketWithData(userName);
+            //OpenSocketWithData(userName);
         }
 
-        private async Task OpenSocketWithData(string userName)
+        public async Task OpenSocketWithData(string userName)
         { 
             if (IsConnected.HasValue && !IsConnected.Value)
             {
@@ -208,10 +376,46 @@ namespace Client_WPF.Windows
 
         private async void ConnectCallback(IAsyncResult ar)
         {
-            OnNetworkStatusChanged?.Invoke(ServerStatus.Connected, MainData);
-            SocketClient.EndConnect(ar);
-            MemStream = new MemoryStream(new byte[StreamCapacity], 0, StreamCapacity, true, true);
-            StartReceive(ar);
+            try
+            {
+                SocketClient.EndConnect(ar);
+                var userName = (string)ar.AsyncState;
+                SendOperationInfo(CodeOperations.Connect, userName);
+                MemStream = new MemoryStream(new byte[StreamCapacity], 0, StreamCapacity, true, true);
+                OnNetworkStatusChanged?.Invoke(ServerStatus.Connected, MainData);
+                StartReceive(ar);
+            }
+            catch (Exception ex)
+            {
+                DisconnectClient(ex);
+            }
+        }
+
+        private void SendOperationInfo(CodeOperations operation, object netdata)
+        {
+            var nd = new NetData()
+            {
+                CodeOperation = operation,
+                Data = netdata
+            };
+            string val = JsonConvert.SerializeObject(nd);
+            try
+            {
+                SocketClient.Client.Send(Encoding.Default.GetBytes(val));
+            }
+            catch (Exception ex)
+            {
+                DisconnectClient(ex);
+            }
+        }
+
+        public void DisconnectClient(Exception ex = null)
+        {
+            if (ex != null)
+                OnNetworkStatusChanged?.Invoke(ServerStatus.Aborted, MainData);
+            else
+                OnNetworkStatusChanged?.Invoke(ServerStatus.Disconnected, MainData);
+            SocketClient?.Dispose();
         }
 
         private void StartReceive(IAsyncResult ar)
@@ -221,40 +425,26 @@ namespace Client_WPF.Windows
 
         private void ReceivedData(IAsyncResult ar)
         {
-            SocketError errorcode = SocketError.Success;
             TcpClient connect = (TcpClient)ar.AsyncState;
-            int bytesRead = connect.Client.EndReceive(ar, out errorcode);
-            if (bytesRead > 0)
+            int bytesRead = connect.Client.EndReceive(ar, out SocketError errorcode);
+            if (bytesRead > 0 && errorcode == SocketError.Success)
             {
-                BinaryReader br = new BinaryReader(MemStream);
-                int code = br.ReadInt32();
-                ParseMessage(code, br, MemStream);
-                ClearPlayerStream();
-                StartReceive(ar);
+                //StartReceive(ar);
             }
-
+            else
+                DisconnectClient();
         }
 
         public static AppNetwork OpenConnectionTo(IPNetData data, string userName)
         {
-            return new AppNetwork(data, userName);
+            return new AppNetwork(data);
         }
 
-        public static AppNetwork OpenConnectionTo(string ipPort, string userName)
+        public static AppNetwork OpenConnectionTo(string ipPort)
         {
-            return new AppNetwork(ipPort, userName);
+            return new AppNetwork(ipPort);
         }
 
-        public bool? IsConnected
-        {
-            get
-            {
-                if (MainData == null || SocketClient == null)
-                    return null;
-                return 
-                    SocketClient?.Connected;
-            }
-        }
 
     }
 
@@ -262,4 +452,5 @@ namespace Client_WPF.Windows
     {
         Disconnected, Aborted, Connected, TryToConnect
     }
+
 }
---
+++ NetDataLibrary/NetData.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NetDataLibrary
+{
+    public class NetData
+    {
+        public CodeOperations CodeOperation { get; set; }
+
+        public object Data { get; set; }
+
+    }
+
+    public enum CodeOperations
+    {
+        Connect, SendMessage, LoadChat, Disconnect
+    }
+
+    public enum ClientStatus
+    {
+        Connected, Disconnected
+    }
+
+    public class Client
+    {
+        public ushort ClientID { get; set; }
+
+        public string UserName { get; set; }
+
+        public ObservableCollection<MessageData> Messages { get; set; }
+
+        public Client(ushort clID, string userName, ObservableCollection<MessageData> messages = null)
+        {
+            ClientID = clID;
+            UserName = userName;
+            if (messages == null)
+            {
+                Messages = new ObservableCollection<MessageData>();
+            }
+            else
+                Messages = messages;
+        }
+
+        public override string ToString()
+        {
+            return UserName;
+        }
+
+        public void AddMessage(ushort recepientID, ushort receiverID, string message)
+        {
+            Messages.Add(new MessageData()
+            {
+                Message = message,
+                ReceiverID = receiverID,
+                RecepientID = recepientID
+            });
+        }
+
+    }
+
+    public class MessageData
+    {
+        public ushort RecepientID { get; set; }
+
+        public ushort ReceiverID { get; set; }
+
+        public string Message { get; set; }
+
+    }
+
+}
