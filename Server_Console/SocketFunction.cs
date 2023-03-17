f9176573a5394b32c7a0d70890f497c35f55153d
Vladosicc <belov.vladosicc@yandex.ru>
2023-03-17 22:37:44 +0300

    Added socket extensions

--- Server_Console/SocketFunction.cs
+++ Server_Console/SocketFunction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
 
-namespace safeboard_2_service.MySocket
+namespace Server_Console
 {
     /// <summary>
     /// Вспомогательный класс для облегчения работы с сокетом
