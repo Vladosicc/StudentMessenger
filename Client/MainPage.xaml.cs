b360dae59392051ba31d969e93e5f1ce9684b85f
Виктор <deather52rus@yandex.ru>
2023-03-23 23:55:26 +0300

    - client added - new MainPage - SignalR initialized (simple)

--- Client/MainPage.xaml.cs
+++ Client/MainPage.xaml.cs
@@ -1,24 +1,99 @@
-п»їnamespace Client;
+using System.Net;
+using System.Net.Sockets;
+
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Client;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+    HubConnection mainConnect;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void B_SendMessage_Clicked(object sender, EventArgs e)
+    {
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void B_ConnectToServer_Clicked(object sender, EventArgs e)
+    {
+        string ip = TB_IPInput.Text;
+        string[] values = ip.Split(":");
+        if (values.Length < 2)
+        {
+            L_ErrorsData.Text = "Ошибка. Введите IP вида 192.168.0.100:{port}";
+            return;
+        }
+        if (IPAddress.TryParse(values[0], out IPAddress ipVal))
+        {
+            if (short.TryParse(values[1], out short port))
+            {
+                mainConnect = new HubConnectionBuilder().WithUrl(string.Format("http://{0}/chat", ip)).Build();
+                mainConnect.Closed += MainConnect_Closed;
+                TryConnectToServer();
+            }
+            
+        }
+    }
+
+    private Task MainConnect_Closed(Exception arg)
+    {
+        L_ErrorsData.Text = arg.Message;
+        B_ConnectToServer.IsEnabled = true;
+        return Task.CompletedTask;
+    }
+
+    private async Task TryConnectToServer()
+    {
+        if (mainConnect != null)
+        {
+            try
+            {
+                B_ConnectToServer.IsEnabled = false;
+                TB_IPInput.IsEnabled = false;
+                await mainConnect.StartAsync();
+                B_SendMessage.IsEnabled = true;
+                TB_LoginInput.IsEnabled = true;
+                TB_MessageInput.IsEnabled = true;
+            }
+            catch (SocketException ex)
+            {        
+                if (ex.SocketErrorCode == SocketError.ConnectionAborted)
+                {
+                    L_ErrorsData.Text = "Ошибка... Невозможно подключиться к серверу.";
+                }
+                B_ConnectToServer.IsEnabled = true;
+                TB_IPInput.IsEnabled = true;
+                B_SendMessage.IsEnabled = false;
+                TB_LoginInput.IsEnabled = false;
+                TB_MessageInput.IsEnabled = false;
+            }
+            catch (HttpRequestException ex)
+            {
+                switch (ex.StatusCode)
+                {
+                    case null:
+                        {
+                            L_ErrorsData.Text = "Ошибка... Невозможно подключиться к серверу.";
+                            break;
+                        }
+                    case HttpStatusCode.Forbidden:
+                        {
+                            L_ErrorsData.Text = "Ошибка... Невозможно подключиться к серверу.";
+                            break;
+                        }
+                }
+                
+                B_ConnectToServer.IsEnabled = true;
+                TB_IPInput.IsEnabled = true;
+                B_SendMessage.IsEnabled = false;
+                TB_LoginInput.IsEnabled = false;
+                TB_MessageInput.IsEnabled = false;
+
+            }
+        }
+    }
 }
-
---
+++ Client/OldPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Client
+{
+	public partial class OldPage : ContentPage
+	{
+		int count = 0;
+		HubConnection connection;
+
+		public OldPage()
+		{
+			InitializeComponent();
+			connection = new HubConnectionBuilder().WithUrl("https://localhost:9678/chat").Build();
+			connection.On<string, string>("Receive", (user, message) =>
+				Dispatcher.Dispatch(() =>
+				{
+					var newMess = $"{user}: {message}";
+
+				}));
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+
+			if (count == 1)
+				CounterBtn.Text = $"Clicked {count} time";
+			else
+				CounterBtn.Text = $"Clicked {count} times";
+
+			SemanticScreenReader.Announce(CounterBtn.Text);
+		}
+
+        private void ContentPage_Loaded(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
